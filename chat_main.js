540c831e857d5b4d47d4d30080489f363783babf
BarisCeliktutan <barisceliktutan@hotmail.com>
2023-07-12 23:10:23 +0100

    update

--- chat_main.js
+++ chat_main.js
@@ -1,35 +1,40 @@
 const body = document.querySelector(".container")
-var text = document.getElementById("entry")
+var textEnt = document.getElementById("entry")
+var userNameEnt = document.querySelector(".user_name_ent")
 const sendBtn = document.querySelector(".send")
 const backBtn = document.querySelector(".go-back")
 var texts = ''
 const addTheText = document.querySelector('.texts')
 
-
-function getText() {
-    return text.value
+function getText(ent) {
+    return ent.value
 }
 
 function displayText() {
-    const text = getText()
-    if (text[0] === "A") {
-        texts += `<p class="user1 text">${text.substring(1)}</p>`
-        addTheText.innerHTML = texts
-    } else if (text[0] === "B")
-    {
-        texts += `<p class="user2 text">${text.substring(1)}</p>`
-        addTheText.innerHTML = texts
+    const text = getText(textEnt)
+    const userName = getText(userNameEnt)
+
+    if (text != "") {
+        if (userName === "Baris") {
+            texts += `<p class="user1 text">${text}</p>`
+            addTheText.innerHTML = texts
+        } else if (userName === "Gokhan")
+        {
+            texts += `<p class="user2 text">${text}</p>`
+            addTheText.innerHTML = texts
+        }
+        textEnt.value = ""
     }
 }
 
 sendBtn.addEventListener('click', displayText)
-sendBtn.addEventListener('keyup', function(event){
+body.addEventListener('keyup', function(event){
     if (event.key == "Enter") {
         displayText
       }
 })
 
-
 backBtn.addEventListener('click', function () {
     body.classList.add('hidden')
 })
+
